0536e425c151ad4a7088cadd0b2223652b8bff18
ken <harygato3@gmail.com>
2020-12-31 13:11:41 -0600

    added more padding to hero

--- components/Hero.js
+++ components/Hero.js
@@ -1,18 +1,20 @@
 export default function Hero({portfolio}){
     return(
-        <div className="py-20 hero-stn_12">
+        <div className="py-32 hero-stn_12">
             <div className="container mx-auto px-6 text-center">
                 <h2 className="text-4xl mb-2 black-white l-font_ppns">
                     Ken Sanchez
                 </h2>
                 <h5 className="text-xl mb-8 text-gray-400">
-                    Software Engineer - Web Developer
+                    Software Engineer >_ Web Developer
                 </h5>                
             </div>
-            <div class="flex items-center ">
-                <svg className="animate-bounce w-6 h-6 text-gray-900" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
-                    <path d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
-                </svg>
+            <div className="grid justify-items-center">
+                <div>
+                    <svg className="animate-pulse w-6 h-6 text-gray-900" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor">
+                        <path d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
+                    </svg>
+                </div>
             </div>
         </div>
     )
