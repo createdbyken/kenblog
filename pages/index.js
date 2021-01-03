454a853e04ba308a2100f7fd88a81977a4e9afc3
ken <harygato3@gmail.com>
2021-01-02 21:38:46 -0600

    updated index.js with new icons

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import utilStyles from '../styles/utils.module.css'
+import Layout from '../components/layout'
 import { getSortedPostsData } from '../lib/posts'
 import Link from 'next/link'
 import Date from '../components/date'
 import Hero from '../components/Hero'
 
+
 export default function Home({ allPostsData }) {
 	return (
-		<>
+		<Layout>
 			<Hero></Hero>
 
 			<section className="container mx-auto px-6 p-10">
@@ -14,11 +16,11 @@ export default function Home({ allPostsData }) {
 					<div className="w-full md:w-1/2">
 						<h1 className="text-5xl text-gray-800 mb-3">✌🏼 ¡Hola! Soy Ken, </h1>
 						<p>
-						soy un Fullstack developer y actualmente trabajo <a target="_blank" href="https://kingtide.com" className="cursor-pointer">@kingtide</a> 🌊. 
+							soy un Fullstack developer y actualmente trabajo <a target="_blank" href="https://kingtide.com" className="cursor-pointer">@kingtide</a> 🌊.
 						Empecé a programar cuando tenía 15 años, recuerdo que mi primer lenguaje de programación fue Ruby 💎 y posteriormente python 🐍. Tengo 23 años, soy Méxicano 🇲🇽 y vivo en <a href="" className="cursor-pointer">Tepoztlán, Morelos</a> ⛰. Mi pasión es ayudar a starups a crear plataformas web escalables y arquitectura solidas. ⛵️
 						</p>
 					</div>
-					
+
 					<div className="w-full md:w-1/2 grid justify-items-center">
 						<img
 							src="/images/profile.jpeg"
@@ -28,33 +30,48 @@ export default function Home({ allPostsData }) {
 					</div>
 				</div>
 
-
 				<div className="flex items-center flex-wrap my-40">
 					<div className="w-full md:w-1/2">
-						<p className="text-7xl md:text-8xl lg:text-9xl text-gray-800 mb-3 trns_text_blk">
-							BACK<br/>END
+						<p 
+							className="text-7xl md:text-8xl lg:text-9xl text-gray-800 mb-3 trns_text_blk">
+							BACK<br />END
 						</p>
+						<div className="grid grid-cols-2 lg:grid-cols-3 gap-3 mt-3">
+							<div><i className="devicon-ruby-plain devIconStyleBlk"></i></div>
+							<div><i className="devicon-python-plain devIconStyleBlk"></i></div>
+							<div><i className="devicon-rails-plain devIconStyleBlk"></i></div>
+							<div><i className="devicon-postgresql-plain devIconStyleBlk"></i></div>
+							<div><i className="devicon-nodejs-plain devIconStyleBlk"></i></div>
+							<div><i className="devicon-java-plain devIconStyleBlk"></i></div>
+						</div>
 					</div>
 					<div className="w-full md:w-1/2">
-						<i className="devicon-aftereffects-plain colored"></i>
-					</div>
-				</div>
-				
-				<div className="flex items-center flex-wrap mb-20 bg-black">
-					<div className="w-full md:w-1/2">
-						<p className="text-white">
-							lorem ipsumlorem ipsumlorem ipsumlorem ipsum lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum
-							lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum
-							lorem ipsumlorem ipsumlorem ipsumlorem ipsum
-						</p>
-					</div>
-					<div className="w-full md:w-1/2">
-						<p className="text-7xl md:text-8xl lg:text-9xl mb-3 trns_text_wht">
-							FRONT<br/>END
+						<p className="text-black text-2xl">
+							Mi pasión por el backend comenzó a inicios de mis primeros años como developer. Diariamente me empujo a nuevas habilidades y retos que me ayudarán a ser un mejor Backend developer.
 						</p>
 					</div>
 				</div>
 			</section>
+
+			<div className="flex items-center flex-wrap min-w-full bg-black">
+				<div className="w-full md:w-1/2 order-last">
+					<p className="text-white mx-8 text-2xl">
+						A lo largo de mi carrera he usado estas tecnologías de forma general en el desarrollo de proyectos con empresas. Complementar estas tecnologías con el modelo Backend me ha ayudado a tener habilidades más sólidas.
+					</p>
+				</div>
+				<div className="w-full md:w-1/2 mb-20 md:order-last">
+					<p className="text-7xl md:text-8xl lg:text-9xl mb-3 trns_text_wht">
+						FRONT<br />END
+					</p>
+					<div className="grid grid-cols-2 lg:grid-cols-3 gap-3 mt-3">
+						<div><i className="devicon-html5-plain devIconStyleWht"></i></div>
+						<div><i className="devicon-css3-plain devIconStyleWht"></i></div>
+						<div><i className="devicon-javascript-plain devIconStyleWht"></i></div>
+						<div><i className="devicon-react-plain devIconStyleWht"></i></div>
+						<div><i className="devicon-gatsby-plain devIconStyleWht"></i></div>
+					</div>
+				</div>
+			</div>
 
 			<div className="bg-gray-100 ">
 				<div className="container mx-auto px-6 text-center py-20">
@@ -63,9 +80,9 @@ export default function Home({ allPostsData }) {
 					</h2>
 
 					<section>
-						<div className="grid grid-cols-3 gap-4">
+						<div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
 							{allPostsData.map(({ id, date, title }) => (
-								<div className="...  md:shadow-lg rounded bg-white pt-5 pb-5" key={id}>
+								<div className="shadow-lg rounded bg-white pt-5 pb-5" key={id}>
 									<Link href={`/posts/${id}`}>
 										<a className="text-xl antialiased text-gray-700	my-14">{title}</a>
 									</Link>
@@ -79,15 +96,15 @@ export default function Home({ allPostsData }) {
 					</section>
 				</div>
 			</div>
-		</>
-	)	
+		</Layout>
+	)
 }
 
 export async function getStaticProps() {
 	const allPostsData = getSortedPostsData()
 	return {
-	  props: {
-		allPostsData
-	  }
+		props: {
+			allPostsData
+		}
 	}
 }
