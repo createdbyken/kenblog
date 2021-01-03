c14fe7240c47a2f998b685ee21d28713014f1503
ken <harygato3@gmail.com>
2021-01-02 22:45:18 -0600

    added footer

---
+++ components/Footer.js
@@ -0,0 +1,14 @@
+export default function Footer(){
+    return (
+        <footer className="bg-gray-100">
+            <div className="container mx-auto px-6 pt-10 pb-6">
+                <div className="flex ">
+                    <div className="w-full md:w-1/4 text-center md:text-left">
+                        <h5 className="uppercase mb-6 font-bold">Links</h5>
+                        
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
+}
