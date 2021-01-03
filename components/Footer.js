02b1820e9ab43b677cbe364784030d91288cecac
ken <harygato3@gmail.com>
2021-01-03 04:13:33 -0600

    cleaned footer site

--- components/Footer.js
+++ components/Footer.js
@@ -1,14 +1,34 @@
 export default function Footer(){
     return (
-        <footer className="bg-gray-100">
-            <div className="container mx-auto px-6 pt-10 pb-6">
-                <div className="flex ">
-                    <div className="w-full md:w-1/4 text-center md:text-left">
-                        <h5 className="uppercase mb-6 font-bold">Links</h5>
-                        
-                    </div>
-                </div>
-            </div>
+        <footer class="flex items-center px-8 py-0 mx-auto bg-gray-100 md:px-8 md:py-4 md:flex-row">
+            <a
+                href="https://github.com/chepetime"
+                class="px-3 py-3 mb-4 text-center text-gray-600 transition-colors duration-500 ease-in-out rounded-lg md:mr-4 md:ml-0 md:bg-gray-100 text-md md:text-md md:p-3 hover:bg-gray-300"
+                target="_blank"
+            >
+                Github
+            </a>
+            <a
+                href="https://linkedin.com/in/jos%C3%A9-manuel-gulias-lugo-03454356/"
+                class="px-3 py-3 mb-4 text-center text-gray-600 transition-colors duration-500 ease-in-out rounded-lg md:mr-4 md:ml-0 md:bg-gray-100 text-md md:text-md md:p-3 hover:bg-gray-300"
+                target="_blank"
+            >
+                LinkedIn
+            </a>
+            <a
+                href="https://instagram.com/chepe/"
+                class="px-3 py-3 mb-4 text-center text-gray-600 transition-colors duration-500 ease-in-out rounded-lg md:mr-4 md:ml-0 md:bg-gray-100 text-md md:text-md md:p-3 hover:bg-gray-300"
+                target="_blank"
+            >
+                Instagram
+            </a>
+            <a
+                href="https://twitter.com/Chepe"
+                class="px-3 py-3 mb-4 text-center text-gray-600 transition-colors duration-500 ease-in-out rounded-lg md:mr-4 md:ml-0 md:bg-gray-100 text-md md:text-md md:p-3 hover:bg-gray-300"
+                target="_blank"
+            >
+            Twitter
+        </a>
         </footer>
     )
 }
