9e9a1718f7766a7914ebc3965c0ad02726e3c496
ken <harygato3@gmail.com>
2021-02-07 11:22:37 -0600

    removed unnecessary code from hero

--- components/Hero.js
+++ components/Hero.js
@@ -1,4 +1,4 @@
-export default function Hero({portfolio}){
+export default function Hero(){
     return(
         <div className="py-32 hero-stn_12">
             <div className="container mx-auto px-6 text-center">
@@ -6,15 +6,8 @@ export default function Hero({portfolio}){
                     Ken Sanchez
                 </h2>
                 <h3 className="text-xl mb-8 text-gray-500">
-                    Software Engineer _ Web Developer
+                    Software Engineer > Web Developer
                 </h3>                
-            </div>
-            <div className="grid justify-items-center">
-                <div>
-                    <svg className="animate-pulse w-6 h-6 text-gray-900" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor">
-                        <path d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
-                    </svg>
-                </div>
             </div>
         </div>
     )
