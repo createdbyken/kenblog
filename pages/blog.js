b091067bd8b307478c81a9170945a63bcbb15309
ken <harygato3@gmail.com>
2021-03-06 13:51:05 -0600

    added blog posts to blog routes and included images

--- pages/blog.js
+++ pages/blog.js
@@ -1,32 +1,65 @@
-import { getSortedPostsData } from '../lib/posts'
 import Link from 'next/link'
+import groq from 'groq'
+import imageUrlBuilder from '@sanity/image-url'
+import client from '../client'
 import Date from '../components/date'
 
-export default function Blog({ allPostsData }){
+
+function urlFor (source) {
+    return imageUrlBuilder(client).image(source)
+}
+
+const Blog = (props) => {
+    const { posts = [] } = props
+	console.log(props)
     return (
-        <section className="container mx-auto px-6 p-10">
-            <div className="grid grid-cols-3 gap-4">
-                {allPostsData.map(({ id, date, title }) => (
-                    <div className="...  md:shadow-lg rounded bg-white pt-5 pb-5" key={id}>
-                        <Link href={`/posts/${id}`}>
-                            <a className="text-xl antialiased text-gray-700	my-14">{title}</a>
-                        </Link>
-                        <br />
-                        <small className="antialiased text-gray-500">
-                            <Date dateString={date} />
-                        </small>
+		<>		
+            <div className="container mx-auto px-6 text-center py-20">
+                <h2 className="mb-6 text-4xl font-bold text-center text-black">
+                    Blogs Recientes
+                </h2>
+                
+                <section className="container mx-auto px-6 p-10">
+                    <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+                        {posts.map(
+                            ({ _id, title = '', slug = '', _createdAt = '', mainImage = '' }) =>
+                                slug && (
+                                <div className="shadow-lg rounded bg-white pt-5 pb-5" key={_id} >
+                                    {mainImage && (
+                                        <div>
+                                            <Link  href="/post/[slug]" as={`/post/${slug.current}`}>
+                                                <img
+                                                    className="rounded-t cursor-pointer" 
+                                                    imageoptions={{fit: 'max'}}
+                                                    alt={mainImage}
+                                                    src={urlFor(mainImage)
+                                                    .url()}
+                                                />
+                                            </Link>
+                                        </div>
+                                    )}
+
+                                    <Link  href="/post/[slug]" as={`/post/${slug.current}`}>
+                                        <a className="text-xl antialiased text-gray-700 my-14" >{title}</a>
+                                    </Link>{' '}
+                                    <br />
+									<small className="antialiased text-gray-600">
+                                        {_createdAt}
+									</small>
+                                </div>
+                            )
+                        )}
                     </div>
-                ))}
+                </section>
             </div>
-        </section>
+		</>
     )
 }
 
-export async function getStaticProps() {
-	const allPostsData = getSortedPostsData()
-	return {
-	  props: {
-		allPostsData
-	  }
-	}
-}
+Blog.getInitialProps = async () => ({
+	posts: await client.fetch(groq`
+	  *[_type == "post" ]
+	`)
+  })
+
+export default Blog
