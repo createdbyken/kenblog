c9062eb28328b393911d6c1bedf82277bf8d0207
ken <harygato3@gmail.com>
2021-03-21 09:45:25 -0600

    re-design cards for portfolio sites

--- components/Card.js
+++ components/Card.js
@@ -1,15 +1,10 @@
 
 export default function Card({src, title, href }) {
     return(
-        <div className="relative cursor-pointer">
+        <div className="relative cursor-pointer border">
             <a target="_blank" href={href}><img className="rounded" src={src} /></a>
-            <style jsx>{`
-            .overlay {
-                background: linear-gradient(0deg, #6666, transparent);
-            }
-            `}</style>
-            <div className="overlay absolute bottom-0 w-full h-24 px-4 pt-6">
-                <div className="text-white text-lg">{title}</div>
+            <div className="overlay w-full h-24 px-4 pt-8 border-t">
+                <div className="text-black text-lg">{title}</div>
             </div>
         </div>
     );
